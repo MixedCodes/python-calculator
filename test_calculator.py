cd8572829b8952ad2576ffab428d4d3ef9f81edd
Patiparn Tangmongkolpaisan <61509834+MixedCodes@users.noreply.github.com>
2024-11-24 11:04:45 +0000

    fix: correct methods in calculator.py according to the test case results.

--- test_calculator.py
+++ test_calculator.py
@@ -8,8 +8,25 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(2, 2), 4)
 
     # Add the following test methods to the TestCalculator class:
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.calc.subtract(5, 2), 3)
+    
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(3, 7), 21)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(5, 2), 2)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+        self.assertEqual(self.calc.modulo(5, 2), 1)
 
 if __name__ == '__main__':
     unittest.main()
